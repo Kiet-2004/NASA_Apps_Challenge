2d428656be53357d316449db6d4a38d57c9b44a6
Nguyễn Phú Thịnh <120249315+tin12q@users.noreply.github.com>
2025-10-04 17:31:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ def api_generate_answer(req: GenerateAnswerRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
